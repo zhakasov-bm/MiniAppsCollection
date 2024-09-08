5766c5f27be18799bd5d499be3663e833d697987
zhakasov-bm <126884891+zhakasov-bm@users.noreply.github.com>
2024-09-08 20:03:21 +0500

    add views to AppCell

--- MiniAppsCollection/MiniAppsCollection/Modules/Main/View/AppCell.swift
+++ MiniAppsCollection/MiniAppsCollection/Modules/Main/View/AppCell.swift
@@ -11,6 +11,20 @@ final class AppCell: UITableViewCell {
     
     static let reuseId = String(describing: AppCell.self)
     
+    private let appImageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.layer.cornerRadius = 15
+        imageView.clipsToBounds = true
+        return imageView
+    }()
+    
+    private let titleLabel: UILabel = {
+        let label = UILabel()
+        label.font = UIFont.systemFont(ofSize: 24, weight: .semibold)
+        label.textColor = .black
+        return label
+    }()
+    
     override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
         super.init(style: style, reuseIdentifier: reuseIdentifier)
         setupViews()
@@ -22,10 +36,18 @@ final class AppCell: UITableViewCell {
     }
     
     private func setupViews() {
-        
+        contentView.addSubviews([appImageView, titleLabel])
     }
     
     private func setupConstraints() {
-        
+        appImageView.snp.makeConstraints { make in
+            make.top.bottom.equalToSuperview().inset(8)
+            make.leading.equalToSuperview().offset(8)
+            make.size.equalTo(100)
+        }
+        titleLabel.snp.makeConstraints { make in
+            make.top.equalToSuperview().offset(20)
+            make.leading.equalTo(appImageView.snp.trailing).offset(32)
+        }
     }
 }

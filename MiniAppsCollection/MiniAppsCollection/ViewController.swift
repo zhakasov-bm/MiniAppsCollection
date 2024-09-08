cfabb7b45b10771f66f3f0273bfee835c5cc9550
zhakasov-bm <126884891+zhakasov-bm@users.noreply.github.com>
2024-09-08 19:11:52 +0500

    add new xcode project

---
+++ MiniAppsCollection/MiniAppsCollection/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MiniAppsCollection
+//
+//  Created by Bekzhan on 08.09.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

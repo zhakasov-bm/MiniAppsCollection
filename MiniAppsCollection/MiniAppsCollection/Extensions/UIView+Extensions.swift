1e76f145e31bac10e25160b6a7bcf02ad4d86218
zhakasov-bm <126884891+zhakasov-bm@users.noreply.github.com>
2024-09-08 19:36:05 +0500

    create tableView

---
+++ MiniAppsCollection/MiniAppsCollection/Extensions/UIView+Extensions.swift
@@ -0,0 +1,8 @@
+//
+//  UIView+Extensions.swift
+//  MiniAppsCollection
+//
+//  Created by Bekzhan on 08.09.2024.
+//
+
+import Foundation
--- MiniAppsCollection/MiniAppsCollection/Modules/MainViewController.swift
+++ MiniAppsCollection/MiniAppsCollection/Modules/MainViewController.swift
@@ -13,8 +13,43 @@ protocol IMainView: AnyObject {
 
 final class MainViewController: UIViewController {
     var presenter: IMainPresenter?
+    
+    private lazy var tableView: UITableView = {
+        let tableView = UITableView()
+        tableView.backgroundColor = .white
+        tableView.delegate = self
+        tableView.dataSource = self
+        return tableView
+    }()
+    
+    override func viewDidLoad() {
+        setupViews()
+        setupConstraints()
+    }
+    
+    private func setupViews() {
+        
+    }
+    
+    private func setupConstraints() {
+        
+    }
 }
 
 extension MainViewController: IMainView {
     
+}
+
+extension MainViewController: UITableViewDelegate {
+    
+}
+
+extension MainViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 5
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        return UITableViewCell()
+    }
 }
